fc56405bfc55462340e5236739818910c761eabb
chucky-golden <chukschibyke92@gmail.com>
2023-12-08 15:44:42 +0100

    new changes

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const cors = require('cors')
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const proxy = require('express-http-proxy')
+const multer = require('multer');
 
 require('dotenv').config({path: __dirname + '/.env'})
 
@@ -16,10 +17,14 @@ app.use('/admin', proxy('https://admin-dqcw.onrender.com'))
 app.use('/vendors', proxy('https://vendors-jpnc.onrender.com'))
 app.use('/', proxy('https://users-40pl.onrender.com'))
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }))
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+const upload = multer();
+app.use(upload.array());
 
 
 app.listen(PORT, () => {
